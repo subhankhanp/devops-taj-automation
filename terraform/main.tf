5b7dbeeb235756c922422e235831fcbc127c321c
Ubuntu <ubuntu@ip-10-200-7-191.ap-southeast-1.compute.internal>
2023-06-02 15:35:11 +0530

    instance files

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "rorserver" {
     delete_on_termination = true
 }
   provisioner "local-exec" {
-    command = "aws ec2 describe-instances --instance-ids ${self.id} --query 'Reservations[0].Instances[0].InstanceId' --output text > instance_id.txt"
+    command = "aws ec2 describe-instances --instance-ids ${self.id} --query 'Reservations[0].Instances[0].InstanceId' --output text > instance_id.tpl"
   }
   # Optional: Customize the instance tags
   tags = {
@@ -23,12 +23,12 @@ resource "aws_instance" "rorserver" {
   }
 }
 
-data "template_file" "instance_id" {
-  template = file("instance_id.txt")
+data "template_file" "instanceid" {
+  template = file("instance_id.tpl")
 }
 
 
 resource "aws_lb_target_group_attachment" "rortg" {
   target_group_arn = "arn:aws:elasticloadbalancing:ap-southeast-1:992805018141:targetgroup/gds-ind-qa1/942b294902806219"
-  target_id        = data.template_file.instance_id.rendered
+  target_id        = data.template_file.instanceid.rendered
 }
